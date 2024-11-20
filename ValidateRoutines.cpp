6949cbc6d5aae69825d64b48ae103e8f1c7346a0
alphathinktink <alphathinktink@gmail.com>
2024-11-20 13:47:33 -0800

    ll

--- ValidateRoutines.cpp
+++ ValidateRoutines.cpp
@@ -1,7 +1,9 @@
-#ifndef VALIDATE_ROUTINES_CPP
-#define VALIDATE_ROUTINES_CPP
+#include "ValidateRoutines.h"
 
-#include <Arduino.h>
+#include "Arduino.h"
+#include "WString.h"
+
+using namespace arduino;
 
 String OmNomNomSD(String &Text,const String &Delim)
 {
@@ -15,7 +17,43 @@ String OmNomNomSD(String &Text,const String &Delim)
     return Res;
   }
   Res=Text.substring(0,Iso);
-  Text=Text.substring(Ido+dl);
+  Text=Text.substring(Iso+dl);
   return Res;
 }
-#endif VALIDATE_ROUTINES_CPP
+
+bool StringIsInteger(const String &Test)
+{
+  if(Test=="" || Test.isEmpty())return false;
+  unsigned int c=Test.length();
+  for(unsigned int i=0;i<c;i++)
+  {
+    if(!isDigit(Test.charAt(i)))return false;
+  }
+  return true;
+}
+
+bool ValidateIPv4Address(const String &Text)//four dotted quads
+{
+  if(Text.length()>15)return false;
+  if(Text=="" || Text.isEmpty())return false;
+  String Test=Text;
+  String P1=OmNomNomSD(Test,".");
+  String P2=OmNomNomSD(Test,".");
+  String P3=OmNomNomSD(Test,".");
+  String P4=OmNomNomSD(Test,".");
+  if(!Test.isEmpty())return false;
+  if(P1=="" || P1.isEmpty() || P2=="" || P2.isEmpty() || P3=="" || P3.isEmpty() || P4=="" || P4.isEmpty())return false;
+  if(!StringIsInteger(P1))return false;
+  if(!StringIsInteger(P2))return false;
+  if(!StringIsInteger(P3))return false;
+  if(!StringIsInteger(P4))return false;
+  int p1=P1.toInt();
+  int p2=P2.toInt();
+  int p3=P3.toInt();
+  int p4=P4.toInt();
+  if(p1<0 || p1>255)return false;
+  if(p2<0 || p2>255)return false;
+  if(p3<0 || p3>255)return false;
+  if(p4<0 || p4>255)return false;
+  return true;
+}
